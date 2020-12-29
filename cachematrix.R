7cd4bb9fc84623d79e2e353282a8db840dec7ff6
Casey Levitt <69866213+clevitt16@users.noreply.github.com>
2020-12-29 14:16:47 -0800

    implimented matrix cache functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+# this function stores a matrix and its inverse, 
+# and it returns functions for getting and setting
+# the matrix and inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv = NULL
+    getMatrix <- function() {
+        x
+    }
+    setMatrix <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    getInverse <- function() {
+        inv
+    }
+    setInverse <- function(z) {
+        inv <<- z
+    }
+    list(getMatrix = getMatrix, setMatrix = setMatrix, 
+         getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
 
+# given the list of get and set functions for the matrix cached
+# in makeCacheMatrix, this function returns the inverse of the matrix,
+# calculating and storing it in the cache if it's not already there
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if (is.null(inv)) {
+        mtrx = x$getMatrix()
+        inv <- solve(mtrx, ...)
+        x$setInverse(inv)
+    }
+    inv
 }
